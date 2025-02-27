88d0b2b7447c563224def663273cc430d9b2ff20
root <root@TheThraw.>
2025-02-27 15:06:57 -0600

    Saving

---
+++ main.py
@@ -0,0 +1,8 @@
+import pygame
+import constants
+
+def main():
+    print(f"Starting Asteroids!\nScreen width: {constants.SCREEN_WIDTH}\nScreen height: {constants.SCREEN_HEIGHT}")
+
+if __name__ == "__main__":
+    main()
