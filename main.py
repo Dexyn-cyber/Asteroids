7cfa762eb39f410162a0050ce6ad5d80225d9df2
root <root@TheThraw.>
2025-02-28 13:59:06 -0600

    checkpoint save, everything is working now

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
 import pygame
-import constants
+from constants import *
+from player import Player
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
-    print(f"Starting Asteroids!\nScreen width: {constants.SCREEN_WIDTH}\nScreen height: {constants.SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    fps_Clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0, 0, 0)) 
+            
+        updatable.update(dt)
+
+        screen.fill((0, 0, 0)) # Also: screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
+        
+        # Limit Framerate to 60 FPS
+        dt = fps_Clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
